d9d088c7a8afc72a86669b3f54756f7796d252f5
Ghian Del Mundo <dmobjects@gmail.com>
2022-06-10 18:14:27 -0700

    Setup production build and server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+// match all unmatched routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+}); 
+
+// express listens to port 3000
+app.listen(port, () => {
+    console.log('Server is up in: ', publicPath);
+});
